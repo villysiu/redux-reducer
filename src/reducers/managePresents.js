ae3012f1b7737e69aee32780ddb72c6f5f1ecad4
villysiu <villysiu@gmail.com>
2021-07-30 16:26:32 -0700

    commit tree 328c2a1ccbed0b6a2a3a14a2c5bb564c5a960b91

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
 export function managePresents(state, action){
+
+    switch (action.type) {
+        case 'INCREASE':
+          return {numberOfPresents: state.numberOfPresents + 1}
+
+        default:
+            return state
+       //   return {numberOfPresents: 0};
+      }
 }
